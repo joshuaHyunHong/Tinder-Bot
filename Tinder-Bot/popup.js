dc8887d82b54a3f4946b987e136b9279826d38dd
joshuaHyunHong <106629094+joshuaHyunHong@users.noreply.github.com>
2023-08-04 19:48:48 -0600

    Update popup.js

--- Tinder-Bot/popup.js
+++ Tinder-Bot/popup.js
@@ -7,11 +7,16 @@ document.getElementById("clickButton").addEventListener("click", () => {
             files: ['content.js']
         }).then(() => {
             console.log('Script executed');
+            // Send a message to the content script to perform the action
+            chrome.tabs.sendMessage(tabs[0].id, { action: "clickButton" });
         }).catch(err => {
-            console.error(err);
+            console.error("popup err: ", err);
         });
     });
 });
+
+
+
 
 
 // make it so that u interact wt popup and click on a btn to find the btn and have the extension auto-click on it? Or just debug content.js ig
