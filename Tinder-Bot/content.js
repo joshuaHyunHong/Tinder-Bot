b3b3e4f38c5dc1b52057f1888732025d737d4168
joshuaHyunHong <106629094+joshuaHyunHong@users.noreply.github.com>
2023-08-04 19:48:29 -0600

    Update content.js

--- Tinder-Bot/content.js
+++ Tinder-Bot/content.js
@@ -3,19 +3,25 @@
 // if(button) button.click();
 
 //https://www.youtube.com/watch?v=IdQNasXktmM at 3:38
+function messageListener(message, sender, sendResponse) {
+    console.log("Message received:", message);
 
-window.onload = function() {
-    let observer = new MutationObserver(function(mutations) {
-      let button = document.querySelector('[class="button Lts($ls-s) Z(0) CenterAlign Mx(a) Cur(p) Tt(u) Bdrs(50%) P(0) Fw($semibold) focus-button-style Bxsh($bxsh-btn) Expand Trstf(e) Trsdu($normal) Wc($transform) Pe(a) Scale(1.1):h Scale(.9):a Bgi($g-ds-background-like):a"]');
-      if (button) { 
-        button.click();
-        console.log("button clicked by content.js");
-        window.alert("btn clicked");
-        observer.disconnect(); // stop observing once the button is found and clicked
-      }
-    });
-  
-    // Start observing the entire document and child elements for changes
-    observer.observe(document, { childList: true, subtree: true });
-  };
+    if (message.action === "clickButton") {
+        let button = document.querySelector('[class="button Lts($ls-s) Z(0) CenterAlign Mx(a) Cur(p) Tt(u) Bdrs(50%) P(0) Fw($semibold) focus-button-style Bxsh($bxsh-btn) Expand Trstf(e) Trsdu($normal) Wc($transform) Pe(a) Scale(1.1):h Scale(.9):a Bgi($g-ds-background-like):a"]');
+        if (button) {
+            button.click();
+            console.log("button clicked by content.js");
+            window.alert("btn clicked");
+
+            // Remove the listener since we've handled the action
+            chrome.runtime.onMessage.removeListener(messageListener);
+        } else {
+            window.alert("NO BTN");
+        }
+    }
+}
+
+chrome.runtime.onMessage.addListener(messageListener);
+
+
   
